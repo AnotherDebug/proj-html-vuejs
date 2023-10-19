e969cab0c9ec4875aacd66031585861c95cb35b1
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-19 17:29:39 +0200

    [ADD]: Navbar component

---
+++ src/components/partials/Navbar.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+
+<template>
+    <div class="navBar d-flex">
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ABOUT</a></li>
+            <li><a href="#">SERVICES</a></li>
+            <li><a href="#">WORK</a></li>
+            <li><a href="#">ARTICLES</a></li>
+        </ul>
+
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+@use '../../scss/partials/variables' as *;
+.navBar {
+    align-items: center;
+    margin-left: 250px;
+
+    ul {
+        list-style: none;
+        padding-right: 15px;
+
+        li {
+            display: inline-block;
+            padding: 0 15px;
+
+            a {
+                text-decoration: none;
+                color: $dimgray-color;
+            }
+        }
+    }
+}</style>
