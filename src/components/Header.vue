0df5bea11a9b9afa4604ce1fbd1e7e71a82420ef
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-20 11:20:12 +0200

    [ADD]: del jumbotron in Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,24 +11,16 @@ export default {
 
 <template>
     <header>
+
         <div class="container-big d-flex">
             <div class="logo">
                 <img src="../assets/images/construction_logo.png" alt="">
             </div>
+
             <Navbar />
+
             <div class="button">
                 <button>get quote</button>
-            </div>
-        </div>
-        <div class="jumbotron">
-            <img src="../assets/images/home-slider-62528242.jpg" alt="">
-            <div class="centeredText">
-                <h1>building inspiring spaces</h1>
-                <p>we build inspiring residential & commercial spaces</p>
-                <div class="buttonJumbo">
-                    <button>explore recent work</button>
-                    <button>get a quote today</button>
-                </div>
             </div>
         </div>
     </header>
@@ -55,6 +47,7 @@ header {
             border: none;
             cursor: pointer;
             text-transform: uppercase;
+            transition: all 0.3S;
 
             &:hover {
                 background-color: $dimgray-color;
@@ -63,61 +56,5 @@ header {
             }
         }
     }
-
-    .jumbotron {
-        position: relative;
-        text-transform: uppercase;
-        text-align: center;
-        width: 100%;
-
-        img {
-            width: 100%;
-        }
-
-        .centeredText {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-
-            p {
-                text-transform: capitalize;
-                margin: 20px 0;
-                color: $yellow-color;
-            }
-
-            .buttonJumbo {
-                button:first-child {
-                    padding: 15px 35px;
-                    background-color: $dimgray-color;
-                    color: $white-color;
-                    border: none;
-                    cursor: pointer;
-                    text-transform: uppercase;
-                    margin: 7px;
-
-                    &:hover {
-                        background-color: $yellow-color;
-                        color: $dimgray-color;
-                        text-transform: uppercase;
-                    }
-                }
-                button:last-child {
-                    padding: 15px 35px;
-                    background-color: $yellow-color;
-                    color: $dimgray-color;
-                    border: none;
-                    cursor: pointer;
-                    text-transform: uppercase;
-                    margin: 7px;
-
-                    &:hover {
-                        background-color: $dimgray-color;
-                        color: $white-color;
-                        text-transform: uppercase;
-                    }
-                }
-            }
-        }
-    }
-}</style>
+}
+</style>
