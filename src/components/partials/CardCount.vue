23a8e1c8e2ddb4ec9d8595d0898db955075932c9
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-21 13:03:42 +0200

    [ADD]: CardCount component

---
+++ src/components/partials/CardCount.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+    name: 'CardCount'
+}
+</script>
+
+
+<template>
+    <div class="counter">
+        <i class="fa-solid fa-suitcase fa-2xl"></i>
+        <p class="countNum">3534</p>
+        <p class="title">PLANNING APPLICATIONS</p>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+@use '../../scss/partials/variables' as *;
+.counter {
+                color: $yellow-color;
+                text-align: center;
+
+
+                i {
+                    margin-bottom: 30px;
+                }
+
+                .countNum {
+                    font-size: 25px;
+                }
+
+                .title {
+                    color: $white-color;
+                    margin: 0 50px;
+                }
+            }
+</style>
