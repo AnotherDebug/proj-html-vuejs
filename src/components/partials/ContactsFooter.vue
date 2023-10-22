3356470d34fea9faf4a08a4d57451a283441c6cf
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-22 19:23:05 +0200

    [ADD]: ContactsFooter component

---
+++ src/components/partials/ContactsFooter.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'ContactsFooter'
+}
+</script>
+
+
+<template>
+    <li class="d-flex">
+        <i class="fa-solid fa-globe"></i>
+        <p>Corporate Location
+            1600 Amphitheatre Parkway
+            London WC1 1BA</p>
+    </li>
+</template>
+
+
+<style lang="scss" scoped>
+
+@use '../../scss/partials/variables' as *;
+
+li {
+    color: lighten($dimgray-color, $amount: 20%);
+    margin-bottom: 10px;
+
+    i {
+        padding-right: 15px;
+    }
+}
+</style>
