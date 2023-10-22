db6281e4291a47a45b8f6daa071927e75b8f460d
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-22 19:40:11 +0200

    [ADD]: LinksNav component

---
+++ src/components/partials/LinksNav.vue
@@ -0,0 +1,45 @@
+<script>
+export default {
+    name: 'LinksNav'
+}
+</script>
+
+
+<template>
+    <li><a href="#">HOME</a></li>
+</template>
+
+
+<style lang="scss" scoped >
+@use '../../scss/partials/variables' as *;
+
+li {
+
+    display: inline-block;
+    padding: 0 15px;
+
+    a {
+        position: relative;
+        text-decoration: none;
+        color: $dimgray-color;
+
+        &:hover {
+            color: $yellow-color;
+            transition: all 0.2s;
+
+            &:after {
+                position: absolute;
+                content: '\2bc6';
+                color: $white-color;
+                font-size: 20px;
+                top: 59px;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 3;
+
+            }
+        }
+
+    }
+}
+</style>
