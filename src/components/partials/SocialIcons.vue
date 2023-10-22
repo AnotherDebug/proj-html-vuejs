0886623dc2b10c8b77314368e9b7c689887ca218
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-22 19:29:30 +0200

    [ADD]: SocialIcons component

---
+++ src/components/partials/SocialIcons.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'SocialIcons'
+}
+</script>
+
+
+<template>
+    <li>
+        <a href="#" title="Facebook"><i class="fa-brands fa-facebook-f"></i></a>
+    </li>
+</template>
+
+
+<style lang="scss" scoped>
+
+@use '../../scss/partials/variables' as *;
+
+li {
+    list-style: none;
+
+    a {
+        position: relative;
+        margin-left: 20px;
+        color: lighten($dimgray-color, $amount: 20%);
+
+        &:hover {
+            color: lighten($black-color, $amount: 10%);
+            transition: 0.5s;
+        }
+    }
+}
+</style>
