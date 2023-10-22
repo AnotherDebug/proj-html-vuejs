a2a0b7f3bbd5b77ea429d8eeb4b604088c50dea5
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-22 12:23:39 +0200

    [ADD]: CardPartner component

---
+++ src/components/partials/CardPartner.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'CardPartner'
+}
+</script>
+
+
+<template>
+    <div class="cardPartner">
+        <img src="home-logo1-219096700-320x202.png" alt="">
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.cardPartner {
+    width: 100%;
+    padding: 20px;
+
+    img {
+        width: 100%;
+    }
+}
+</style>
