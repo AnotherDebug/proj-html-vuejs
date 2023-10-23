d3ebcc9c03e1f7a22c9b9fe0cf51a58179d8e1f9
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-23 18:10:34 +0200

    [ADD]: method startCount

--- src/components/partials/CardCount.vue
+++ src/components/partials/CardCount.vue
@@ -3,6 +3,30 @@ export default {
     name: 'CardCount',
     props: {
         count: Object
+    },
+    data() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        startCount(total) {
+            //settaggio tempo del setInterval
+            let intervalTime = total > 3000 ? 0.1 : 1;
+
+            const interval = setInterval(() => {
+                if (this.counter < total) {
+                    //settaggio del counter
+                    let increment = total > 3000 ? 3 : 1;
+                    this.counter += increment;
+                } else {
+                    clearInterval(interval);
+                }
+            }, intervalTime);
+        }
+    },
+    mounted() {
+        this.startCount(this.count.numbers);
     }
 }
 </script>
@@ -11,31 +35,31 @@ export default {
 <template>
     <div class="counter">
         <i :class="count.icon"></i>
-        <p class="countNum">{{ count.numbers }}</p>
+        <p class="countNum">{{ counter }}</p>
         <p class="title">{{ count.title }}</p>
     </div>
 </template>
 
 
 <style lang="scss" scoped>
-
 @use '../../scss/partials/variables' as *;
+
 .counter {
-                color: $yellow-color;
-                text-align: center;
-                text-transform: uppercase;
+    color: $yellow-color;
+    text-align: center;
+    text-transform: uppercase;
 
-                i {
-                    margin-bottom: 30px;
-                }
+    i {
+        margin-bottom: 30px;
+    }
 
-                .countNum {
-                    font-size: 25px;
-                }
+    .countNum {
+        font-size: 25px;
+    }
 
-                .title {
-                    color: $white-color;
-                    margin: 5px 50px;
-                }
-            }
+    .title {
+        color: $white-color;
+        margin: 5px 50px;
+    }
+}
 </style>
