0b4bd750a0ffd2ef88df7b833d7af41a5f017c23
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-23 10:57:00 +0200

    [UPDATE]: store data

--- src/data/store.js
+++ src/data/store.js
@@ -142,6 +142,107 @@ export const store = reactive({
 
   ],
 
+  cardsLatest: [
+    {
+      image: "blog-post-134132600.jpg",
+      bodyTitle: "Redeveloping Florida’s Remote Southern Coast",
+      bodyDate: "December 7th, 2015",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore.",
+      href: "#",
+      firstIcon: "fa-solid fa-link",
+      secondIcon: "fa-solid fa-magnifying-glass",
+      title: "Florida Health Facility",
+      text: "Commercial"
+    },
+    {
+      image: "blog-post-92486644.jpg",
+      bodyTitle: "How We Manage Large Construction Projects",
+      bodyDate: "December 7th, 2015",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore.",
+      href: "#",
+      firstIcon: "fa-solid fa-link",
+      secondIcon: "fa-solid fa-magnifying-glass",
+      title: "Florida Health Facility",
+      text: "Commercial"
+    },
+    {
+      image: "blog-post-332773904.jpg",
+      bodyTitle: "Future proofing a modern home",
+      bodyDate: "December 6th, 2015",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore.",
+      href: "#",
+      firstIcon: "fa-solid fa-link",
+      secondIcon: "fa-solid fa-magnifying-glass",
+      title: "Florida Health Facility",
+      text: "Commercial"
+    },
+  ],
+
+  iconPartners: [
+    {
+      icon: "home-logo11-219096700-320x202.png",
+    },
+    {
+      icon: "home-logo10-219096700-320x202.png",
+    },
+    {
+      icon: "home-logo2-219096700-320x202.png",
+    },
+    {
+      icon: "home-logo1-219096700-320x202.png",
+    },
+    {
+      icon: "home-logo9-219096700-320x202.png",
+    },
+  ],
+
+  tweetsList: [
+    {
+      logo: "3a74ce3d0532b7773b174c45ca3bd05a_normal.png",
+      image: "classic_thumbnail.jpg",
+      title: "ThemeFusion",
+      ashTitle: "@Theme_Fusion",
+      icon: "fa-brands fa-twitter",
+      bodyDate: "22h",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore.",
+      like: "fa-regular fa-heart",
+      share: "fa-solid fa-arrow-right-from-bracket",
+    },
+    {
+      logo: "3a74ce3d0532b7773b174c45ca3bd05a_normal.png",
+      image: null,
+      title: "ThemeFusion",
+      ashTitle: "@Theme_Fusion",
+      icon: "fa-brands fa-twitter",
+      bodyDate: "22h",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore.",
+      like: "fa-regular fa-heart",
+      share: "fa-solid fa-arrow-right-from-bracket",
+    },
+    {
+      logo: "3a74ce3d0532b7773b174c45ca3bd05a_normal.png",
+      image: "En2TRxLW4AEiWUN.jpeg",
+      title: "ThemeFusion",
+      ashTitle: "@Theme_Fusion",
+      icon: "fa-brands fa-twitter",
+      bodyDate: "15h",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi, quidem quos voluptates esse? Quis natus quo officiis dolore.",
+      like: "fa-regular fa-heart",
+      share: "fa-solid fa-arrow-right-from-bracket",
+    },
+    {
+      logo: "3a74ce3d0532b7773b174c45ca3bd05a_normal.png",
+      image: "92124892-54cc2e00-edcc-11ea-8994-f15925e53b1e.jpg",
+      title: "ThemeFusion",
+      ashTitle: "@Theme_Fusion",
+      icon: "fa-brands fa-twitter",
+      bodyDate: "2h",
+      bodyText: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, vero tempore minus officiis facere repellat amet a fugit tenetur id quasi!",
+      like: "fa-regular fa-heart",
+      share: "fa-solid fa-arrow-right-from-bracket",
+    },
+  ],
+
   contactsMenu: [
     {
       icon: "fa-solid fa-globe",
@@ -173,18 +274,22 @@ export const store = reactive({
     {
       icon: "fa-brands fa-square-facebook",
       title: "facebook",
+      href: "#"
     },
     {
       icon: "fa-brands fa-square-twitter",
       title: "twitter",
+      href: "#"
     },
     {
       icon: "fa-brands fa-youtube",
       title: "youtube",
+      href: "#"
     },
     {
       icon: "fa-brands fa-instagram",
       title: "youtube",
+      href: "#"
     },
   ],
 });
